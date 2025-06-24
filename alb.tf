f02d6aff2eda08a70fd758de72aff47cfd48b0be
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-24 18:11:27 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "ecs_alb" {
   name               = "${var.project_name}-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.alb_sg.id] 
  subnets = [for s in aws_subnet.public : s.id]
 
   tags = {
