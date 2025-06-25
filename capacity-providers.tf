66cfd0c30f0d6dbebf3d71af1a7f1f84b49a079f
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-25 18:25:33 +0530

    Update capacity-providers.tf

--- capacity-providers.tf
+++ capacity-providers.tf
@@ -1,6 +1,6 @@
 # Web Capacity Provider
 resource "aws_ecs_capacity_provider" "web_cp" {
-  name = "${var.project_name}-web-cp"
+  name = "${var.project_name}-web-cp-custom"
 
   auto_scaling_group_provider {
     auto_scaling_group_arn = aws_autoscaling_group.web_asg.arn
@@ -22,7 +22,7 @@ resource "aws_ecs_capacity_provider" "web_cp" {
 
 # App Capacity Provider
 resource "aws_ecs_capacity_provider" "app_cp" {
-  name = "${var.project_name}-app-cp"
+  name = "${var.project_name}-app-cp-custom"
 
   auto_scaling_group_provider {
     auto_scaling_group_arn = aws_autoscaling_group.app_asg.arn
