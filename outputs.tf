53121fb7933041ba6e104f1718a26e88fbae6e50
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-25 18:14:21 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,7 @@ output "public_subnet_ids" {
 
 output "private_subnet_ids_app" {
   value       = [for s in aws_subnet.private_app : s.id]
+  description = "IDs of app tier private subnets"
 }
 
 output "private_subnet_ids_db" {
@@ -17,32 +18,47 @@ output "private_subnet_ids_db" {
   description = "IDs of DB private subnets"
 }
 
-
 output "alb_dns_name" {
   value       = aws_lb.ecs_alb.dns_name
   description = "DNS name of the Application Load Balancer"
 }
 
 output "rds_endpoint" {
-  description = "The connection endpoint for the RDS instance"
   value       = aws_db_instance.app_db.endpoint
+  description = "RDS instance connection endpoint"
 }
 
 output "ecs_cluster_name" {
   value       = aws_ecs_cluster.ecs_cluster.name
-  description = "Name of the ECS Cluster"
+  description = "ECS Cluster name"
 }
 
-output "ecs_service_name" {
+output "ecs_app_service_name" {
   value       = aws_ecs_service.app.name
-  description = "Name of the ECS Service"
+  description = "ECS App Service name"
 }
 
-output "ecr_repo_url" {
+output "ecs_web_service_name" {
+  value       = aws_ecs_service.web.name
+  description = "ECS Web Service name"
+}
+
+output "web_ecr_repo_url" {
+  value       = aws_ecr_repository.web.repository_url
+  description = "URL of Web ECR Repository"
+}
+
+output "app_ecr_repo_url" {
   value       = aws_ecr_repository.app.repository_url
-  description = "URL of the ECR Repository"
+  description = "URL of App ECR Repository"
 }
 
-output "asg_name" {
-  value = aws_autoscaling_group.ecs_asg.name
+output "web_asg_name" {
+  value       = aws_autoscaling_group.web_asg.name
+  description = "Name of Web Auto Scaling Group"
+}
+
+output "app_asg_name" {
+  value       = aws_autoscaling_group.app_asg.name
+  description = "Name of App Auto Scaling Group"
 }
