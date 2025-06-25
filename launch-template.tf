f55475a69d0495a20aeb079078c8e50670464c9d
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-25 17:08:53 +0530

    Update launch-template.tf

--- launch-template.tf
+++ launch-template.tf
@@ -1,6 +1,6 @@
-resource "aws_launch_template" "ecs_lt" {
-  name_prefix   = "${var.project_name}-lt"
-  image_id = data.aws_ami.ecs_ami.id
+resource "aws_launch_template" "web_lt" {
+  name_prefix   = "${var.project_name}-web-lt"
+  image_id      = data.aws_ami.ecs_ami.id
   instance_type = var.instance_type
   user_data     = base64encode(file("ecs-userdata.sh"))
 
@@ -8,5 +8,32 @@ resource "aws_launch_template" "ecs_lt" {
     name = aws_iam_instance_profile.ecs_instance_profile.name
   }
 
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.project_name}-web"
+    }
+  }
+}
+
+resource "aws_launch_template" "app_lt" {
+  name_prefix   = "${var.project_name}-app-lt"
+  image_id      = data.aws_ami.ecs_ami.id
+  instance_type = var.instance_type
+  user_data     = base64encode(file("ecs-userdata.sh"))
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_instance_profile.name
+  }
+
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.project_name}-app"
+    }
+  }
 }
