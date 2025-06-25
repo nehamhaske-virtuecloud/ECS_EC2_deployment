1e9fcbf93de5a7aa8d14ac10963d9ef2eb0b85e8
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-25 16:56:11 +0530

    Update ecs_task_def_service.tf

--- ecs_task_def_service.tf
+++ ecs_task_def_service.tf
@@ -12,8 +12,8 @@ resource "aws_ecs_task_definition" "app" {
       image = "${aws_ecr_repository.app.repository_url}:latest"
       portMappings = [
         {
-          containerPort = 80
-          hostPort      = 80
+          containerPort = 8080
+          hostPort      = 8080
         }
       ]
     }
@@ -30,11 +30,51 @@ resource "aws_ecs_service" "app" {
   load_balancer {
     target_group_arn = aws_lb_target_group.ecs_tg.arn
     container_name   = "app-container"
+    container_port   = 8080
+  }
+
+  depends_on = [
+    aws_autoscaling_group.ecs_asg
+  ]
+}
+
+
+resource "aws_ecs_task_definition" "web" {
+  family                   = "web-task"
+  network_mode             = "bridge"
+  requires_compatibilities = ["EC2"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "web-container"
+      image = "${aws_ecr_repository.web.repository_url}:latest"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "web" {
+  name            = "web-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.web.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+    container_name   = "web-container"
     container_port   = 80
   }
 
   depends_on = [
     aws_lb_listener.ecs_listener,
-    aws_autoscaling_group.ecs_asg
   ]
 }
