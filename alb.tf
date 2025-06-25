5fafaf246f0e1ff59a7b08c218b84827542a75b5
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-25 17:01:43 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,17 +1,19 @@
+# Application Load Balancer
 resource "aws_lb" "ecs_alb" {
   name               = "${var.project_name}-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id] 
- subnets = [for s in aws_subnet.public : s.id]
- 
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [for s in aws_subnet.public : s.id]
+
   tags = {
     Name = "${var.project_name}-alb"
   }
 }
 
-resource "aws_lb_target_group" "ecs_tg" {
-  name        = "${var.project_name}-tg"
+# Target Group for Web Service (public)
+resource "aws_lb_target_group" "web_tg" {
+  name        = "${var.project_name}-web-tg"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
@@ -28,17 +30,54 @@ resource "aws_lb_target_group" "ecs_tg" {
   }
 
   tags = {
-    Name = "${var.project_name}-tg"
+    Name = "${var.project_name}-web-tg"
   }
 }
 
-resource "aws_lb_listener" "ecs_listener" {
+# Target Group for App Service (private)
+resource "aws_lb_target_group" "app_tg" {
+  name        = "${var.project_name}-app-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-tg"
+  }
+}
+
+# Listener on port 80 → forwards to web_tg by default
+resource "aws_lb_listener" "ecs_web_listener" {
   load_balancer_arn = aws_lb.ecs_alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs_tg.arn
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+
+# Listener on port 8080 → forwards to web_tg by default
+resource "aws_lb_listener" "ecs_app_listener" {
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 8080
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
