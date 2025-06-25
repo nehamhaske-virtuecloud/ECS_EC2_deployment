cbb0869269fb4269ec4337d9596ab677dd4495b6
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-25 18:13:52 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,3 +1,4 @@
+# ALB Security Group
 resource "aws_security_group" "alb_sg" {
   name        = "${var.project_name}-alb-sg"
   description = "Allow HTTP from internet to ALB"
@@ -23,9 +24,10 @@ resource "aws_security_group" "alb_sg" {
   }
 }
 
-resource "aws_security_group" "ec2_sg" {
-  name        = "${var.project_name}-ec2-sg"
-  description = "Allow traffic from ALB"
+# Web EC2 Security Group (allow all inbound from ALB)
+resource "aws_security_group" "web_sg" {
+  name        = "${var.project_name}-web-sg"
+  description = "Allow HTTP from ALB and SSH from anywhere"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -36,12 +38,12 @@ resource "aws_security_group" "ec2_sg" {
     security_groups = [aws_security_group.alb_sg.id]
   }
 
-ingress {
-    description     = "Allow HTTP from ALB"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "ssh"
-    security_groups = ["0.0.0.0/0"]
+  ingress {
+    description = "Allow SSH from internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -52,21 +54,48 @@ ingress {
   }
 
   tags = {
-    Name = "${var.project_name}-ec2-sg"
+    Name = "${var.project_name}-web-sg"
   }
 }
 
-resource "aws_security_group" "rds_sg" {
-  name        = "${var.project_name}-rds-sg"
-  description = "Allow MySQL access from ECS EC2 instances"
+# App EC2 Security Group (allow only from web_sg)
+resource "aws_security_group" "app_sg" {
+  name        = "${var.project_name}-app-sg"
+  description = "Allow HTTP from Web EC2 SG only"
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description     = "Allow MySQL from EC2 SG"
+    description     = "Allow HTTP from web_sg"
+    from_port       = 8080
+    to_port         = 8080
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-sg"
+  }
+}
+
+# RDS Security Group
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project_name}-rds-sg"
+  description = "Allow MySQL access from App EC2 SG only"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow MySQL from App EC2"
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]
+    security_groups = [aws_security_group.app_sg.id]
   }
 
   egress {
