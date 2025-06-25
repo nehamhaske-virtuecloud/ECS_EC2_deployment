4b8ba3fd32a10700afecf7536a3f2187a9cd9ccc
nehamhaske-virtuecloud <neha.mhaske@virtuecloud.io>
2025-06-25 17:21:52 +0530

    Create capacity-providers.tf

---
+++ capacity-providers.tf
@@ -0,0 +1,53 @@
+# Web Capacity Provider
+resource "aws_ecs_capacity_provider" "web_cp" {
+  name = "${var.project_name}-web-cp"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.web_asg.arn
+
+    managed_scaling {
+      status                    = "ENABLED"
+      target_capacity           = 80
+      minimum_scaling_step_size = 1
+      maximum_scaling_step_size = 100
+    }
+
+    managed_termination_protection = "ENABLED"
+  }
+
+  tags = {
+    Name = "${var.project_name}-web-cp"
+  }
+}
+
+# App Capacity Provider
+resource "aws_ecs_capacity_provider" "app_cp" {
+  name = "${var.project_name}-app-cp"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.app_asg.arn
+
+    managed_scaling {
+      status                    = "ENABLED"
+      target_capacity           = 80
+      minimum_scaling_step_size = 1
+      maximum_scaling_step_size = 100
+    }
+
+    managed_termination_protection = "ENABLED"
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-cp"
+  }
+}
+
+
+resource "aws_ecs_cluster_capacity_providers" "attach_cp" {
+  cluster_name = aws_ecs_cluster.ecs_cluster.name
+
+  capacity_providers = [
+    aws_ecs_capacity_provider.web_cp.name,
+    aws_ecs_capacity_provider.app_cp.name
+  ]
+}
